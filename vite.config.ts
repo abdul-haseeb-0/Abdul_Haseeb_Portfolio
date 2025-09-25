522a648dec6b3144df6c2ed1c294836037f41b6e
Haseeb <ah673277@gmail.com>
2025-09-25 11:47:29 +0500

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import path from "path"
+import react from "@vitejs/plugin-react-swc"
+import { defineConfig } from "vite"
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+export default defineConfig({
+  // This is the crucial line for GitHub Pages deployment
+  base: "/Abdul_Haseeb_Portfolio/",
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
     },
   },
-}));
+})
